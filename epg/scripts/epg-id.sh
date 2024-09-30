3263cdb9ffe119799d0a7a1cc6d04ff35da1de67
didikc <166754124+didikc@users.noreply.github.com>
2024-10-01 03:01:44 +0700

    Create epg-id.sh

---
+++ epg/scripts/epg-id.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cd /home/runner/work/didikc/didikc/iptv-org-epg && npm install
+
+# Indihome TV EPG
+
+npm run grab -- --site=indihometv.com --output=../epg/id-indihometv.xml --days=2 --maxConnections=10
+
+# Vision Plus EPG
+
+npm run grab -- --site=visionplus.id --output=../epg/id-visionplus.xml --days=5 --maxConnections=10
+
+# Vidio EPG
+
+npm run grab -- --site=vidio.com --output=../epg/id-vidio.xml --days=5 --maxConnections=10
+
+# Cubmu EPG
+
+npm run grab -- --site=cubmu.com --output=../epg/id-cubmu.xml --days=5 --maxConnections=10
+
+# Dens TV EPG
+
+npm run grab -- --site=dens.tv --output=../epg/id-denstv.xml --days=5 --maxConnections=10
+
+# Compress EPG xml files
+cd ../epg/
+
+gzip -k -f -9 ../epg/id*.xml
+
+# Remove EPG xml files
+
+rm ../epg/id*.xml
+
+exit 0
