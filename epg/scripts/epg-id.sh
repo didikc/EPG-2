5e61a880f27bef80329bc8ab6ffcd1b86924af18
didikc <166754124+didikc@users.noreply.github.com>
2025-01-03 19:38:12 +0700

    Update epg-id.sh

--- epg/scripts/epg-id.sh
+++ epg/scripts/epg-id.sh
@@ -6,14 +6,6 @@ cd /home/runner/work/EPG-2/EPG-2/iptv-org-epg && npm install
 
 npm run grab -- --site=indihometv.com --output=../epg/id-indihometv.xml --days=3 --maxConnections=10
 
-# Vision Plus EPG
-
-npm run grab -- --site=visionplus.id --output=../epg/id-visionplus.xml --days=3 --maxConnections=10
-
-# Vidio EPG
-
-npm run grab -- --site=vidio.com --output=../epg/id-vidio.xml --days=3 --maxConnections=10
-
 # Cubmu EPG
 
 npm run grab -- --site=cubmu.com --output=../epg/id-cubmu.xml --days=3 --maxConnections=10
@@ -22,13 +14,21 @@ npm run grab -- --site=cubmu.com --output=../epg/id-cubmu.xml --days=3 --maxConn
 
 npm run grab -- --site=dens.tv --output=../epg/id-denstv.xml --days=3 --maxConnections=10
 
+# Vision Plus EPG
+
+npm run grab -- --site=visionplus.id --output=../epg/id-visionplus.xml --days=3 --maxConnections=10
+
+# Vidio EPG
+
+# npm run grab -- --site=vidio.com --output=../epg/id-vidio.xml --days=3 --maxConnections=10
+
 # Firstmedia EPG
 
-npm run grab -- --site=firstmedia.com --output=../epg/id-firstmedia.xml --days=3 --maxConnections=10
+# npm run grab -- --site=firstmedia.com --output=../epg/id-firstmedia.xml --days=3 --maxConnections=10
 
 # Singtel EPG
 
-npm run grab -- --site=singtel.com --output=../epg/sg-singtel.xml --days=3 --maxConnections=10
+# npm run grab -- --site=singtel.com --output=../epg/sg-singtel.xml --days=3 --maxConnections=10
 
 # Pix EPG
 
