cc30752ef4a20af6f78b161d34d97510ccf1d9df
didikc <166754124+didikc@users.noreply.github.com>
2026-05-12 08:20:44 +0800

    Update epg-id.sh

--- epg/scripts/epg-id.sh
+++ epg/scripts/epg-id.sh
@@ -3,22 +3,22 @@
 cd /home/runner/work/EPG-2/EPG-2/iptv-org-epg && npm install
 
 # Maxstream EPG
-npm run grab -- --site=maxstream.tv --output=../epg/id-maxstreamtv.xml --days=3
+npm run grab -- --source=maxstream.tv --output=../epg/id-maxstreamtv.xml --days=3
 
 # Cubmu EPG
-npm run grab -- --site=cubmu.com --lang=id --output=../epg/id-cubmu.xml --days=3
+npm run grab -- --source=cubmu.com --lang=id --output=../epg/id-cubmu.xml --days=3
 
 # Dens TV EPG
-npm run grab -- --site=dens.tv --output=../epg/id-denstv.xml --days=3
+npm run grab -- --source=dens.tv --output=../epg/id-denstv.xml --days=3
 
 # Vision Plus EPG
-npm run grab -- --site=visionplus.id --output=../epg/id-visionplus.xml --days=3
+npm run grab -- --source=visionplus.id --output=../epg/id-visionplus.xml --days=3
 
 # Vidio EPG
-npm run grab -- --site=vidio.com --output=../epg/id-vidio.xml --days=3
+npm run grab -- --source=vidio.com --output=../epg/id-vidio.xml --days=3
 
 # Firstmedia EPG
-npm run grab -- --site=firstmedia.com --output=../epg/id-firstmedia.xml --days=3
+npm run grab -- --source=firstmedia.com --output=../epg/id-firstmedia.xml --days=3
 
 # Pix EPG (channels-based, no site)
 npm run grab -- --channels=../epg/scripts/pix.xml --output=../epg/pix.xml --days=3
